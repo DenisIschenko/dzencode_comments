1f95af2baa99ec7ba44aad412acbd0ff82d33e06
Denis Ischenko <denisischenko88@gmail.com>
2025-06-26 19:20:12 +0300

    added logic to generate superuser, because the server do not provide this

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,15 @@ python manage.py migrate
 
 python manage.py collectstatic --noinput
 
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username="${DJANGO_SUPERUSER_USERNAME}").exists():
+    User.objects.create_superuser(
+        username="${DJANGO_SUPERUSER_USERNAME}",
+        email="${DJANGO_SUPERUSER_EMAIL}",
+        password="${DJANGO_SUPERUSER_PASSWORD}"
+    )
+END
+
 uvicorn core.asgi:application --host 0.0.0.0 --port 8000 --workers 4 --timeout-keep-alive 120
